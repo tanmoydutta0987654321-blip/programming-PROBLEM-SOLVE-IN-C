86fd2f0b9a596235a086e61ffc90359181ffb6d8
Tanmoy Dutta <tanmoydutta0987654321@gmail.com>
2026-03-24 13:04:32 +0530

    error

---
+++ chapter-1/q1.c
@@ -0,0 +1,12 @@
+// Write a C program to print the name , age , Address , using specific format
+#include<stdio.h>
+
+int main()
+{
+    char Name; 
+    //int Age; char Address;
+    // printf("Enter your Name :");
+    scanf("%c",&Name);
+    printf("%c",Name);
+    return 0;
+}

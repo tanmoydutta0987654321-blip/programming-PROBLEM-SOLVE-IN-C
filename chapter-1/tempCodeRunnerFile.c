b983c33d56a93b58d576a577166be0103a66a322
Tanmoy Dutta <tanmoydutta0987654321@gmail.com>
2026-03-29 22:42:15 +0530

    two question are pending

---
+++ chapter-1/tempCodeRunnerFile.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+// int main(){
+//     char name[50]; int age; char address[200];
+//     printf("Enter your name :");
+//     fgets(name,sizeof(name),stdin);
+//     printf("Enter your age :");
+//     scanf("%d",&age);
+//     printf("Enter your address:");
+//     fgets(address, sizeof(address), stdin);
+
+//     printf("Name: %s", name);
+//     printf("Age: %d\n", age);
+//     printf("Address: %s", address);
+
+
+
+//     return 0;    
+// }
